0652e071c651eeca36d239557b758c25f33c9303
Devill Don <53294521+devillD@users.noreply.github.com>
2022-05-12 17:54:34 +0600

    Update selfping.sh

--- scriptplusconf/selfping.sh
+++ scriptplusconf/selfping.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 while :
 do
-sleep 1440
+sleep `shuf -i 15-30 -n 1`m
 curl -L "SELFPINGURL"
 curl -L "$PINGDOMAIN"
-
+sleep `shuf -i 1-10 -n 1`m
+curl -A "Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.114 Mobile Safari/537.36" "https://ping-production.up.railway.app/?link=$PINGDOMAIN"
 done
